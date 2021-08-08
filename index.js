bfc546b80da63e65979ab3481d4b22b125c84a2c
THARUN.V <85789403+VTHARUN01@users.noreply.github.com>
2021-08-08 10:44:08 +0530

    CHAT-APP

---
+++ index.js
@@ -0,0 +1,22 @@
+let commentsAll = []
+const express = require('express')
+const app = express()
+const path = require('path')
+const socket = require('socket.io')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, '/public')))
+app.get('/comments/:name', (req, res) => {
+    const { name } = req.params
+    res.render('chat', { name })
+})
+const server = app.listen(3000, () => {
+    console.log('I AM WATCHING U')
+})
+const io = socket(server)
+io.on("connection", (socket) => {
+    console.log('HARDWORK')
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
+})
