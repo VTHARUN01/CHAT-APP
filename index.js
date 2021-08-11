45dce3d91f10ecacc849337a1ba47b2be463f001
THARUN.V <85789403+VTHARUN01@users.noreply.github.com>
2021-08-11 19:07:00 +0530

    CHAT-APP

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const server = app.listen(3000, () => {
 })
 const io = socket(server)
 io.on("connection", (socket) => {
-    console.log('HARDWORK')
     socket.on('chat', (data) => {
         io.sockets.emit('chat', data)
     })
